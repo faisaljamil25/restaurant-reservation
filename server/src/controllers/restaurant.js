4ebf8279ecc292e090f97484547dc2bf20310186
Faisal Jamil <faisaljamilcs@gmail.com>
2021-07-15 02:48:34 +0530

    availablity

--- server/src/controllers/restaurant.js
+++ server/src/controllers/restaurant.js
@@ -38,3 +38,45 @@ export const reservation = async (req, res) => {
     res.status(404).json({ message: error.message });
   }
 };
+
+export const availablity = async (req, res) => {
+  try {
+    console.log('request attempted');
+
+    console.log(req.body);
+    const dateTime = new Date(req.body.date);
+
+    Day.find({ date: dateTime }, (err, docs) => {
+      if (!err) {
+        if (docs.length > 0) {
+          // Record already exists
+          console.log('Record exists. Sent docs.');
+          res.status(200).send(docs[0]);
+        } else {
+          // Searched date does not exist and we need to create it
+          const allTables = require('../data/allTables');
+          const day = new Day({
+            date: dateTime,
+            tables: allTables,
+          });
+          day.save((err) => {
+            if (err) {
+              res.status(400).send('Error saving new date');
+            } else {
+              // Saved date and need to return all tables (because all are now available)
+              console.log('Created new datetime. Here are the default docs');
+              Day.find({ date: dateTime }, (err, docs) => {
+                err ? res.sendStatus(400) : res.status(200).send(docs[0]);
+              });
+            }
+          });
+        }
+      } else {
+        res.status(400).send('Could not search for date');
+      }
+    });
+  } catch (error) {
+    console.log(error.message);
+    res.status(404).json({ message: error.message });
+  }
+};
