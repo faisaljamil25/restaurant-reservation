7b7d020db85dd27e94b7b6e51ad7210226e16d3b
Faisal Jamil <faisaljamilcs@gmail.com>
2021-07-15 03:37:39 +0530

    navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,12 @@
+import React, { useState } from 'react';
+
+import Navbar from './components/navbar';
+
 function App() {
+  const [page, setPage] = useState(0);
   return (
     <div className='App'>
+      <Navbar setPage={setPage} />
       <h1>Welcome to our Restaurant!</h1>
     </div>
   );
