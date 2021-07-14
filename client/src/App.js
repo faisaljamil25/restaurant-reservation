1e178b2e124d52f6779091e0109f03ab67be4d13
Faisal Jamil <faisaljamilcs@gmail.com>
2021-07-15 04:29:17 +0530

    main component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
 
 import Navbar from './components/navbar';
+import Main from './components/main';
 
 function App() {
   const [page, setPage] = useState(0);
   return (
     <div className='App'>
       <Navbar setPage={setPage} />
-      <h1>Welcome to our Restaurant!</h1>
+      {page === 0 ? <Main setPage={setPage} /> : null}
     </div>
   );
 }
---
+++ client/src/components/main.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import { Button, Typography } from '@material-ui/core';
+import LocalPizzaIcon from '@material-ui/icons/LocalPizza';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+}));
+
+const Main = ({ setPage }) => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Typography>
+            If you're looking for great pizza
+            <LocalPizzaIcon />
+          </Typography>
+          <Button
+            color='inherit'
+            onClick={(_) => {
+              setPage(1);
+            }}
+          >
+            Book a Table
+          </Button>
+        </Grid>
+        <Grid item xs={12}>
+          <img src='/cafe.jpg' alt='cafe' />
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Main;
