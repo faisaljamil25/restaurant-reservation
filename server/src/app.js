70211137ad2a8c7036c782d61f177e8468f1167c
Faisal Jamil <faisaljamilcs@gmail.com>
2021-07-15 02:33:24 +0530

    server created

--- server/src/app.js
+++ server/src/app.js
@@ -1,30 +1,33 @@
-require("dotenv").config();
-const express = require("express");
-const path = require("path");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const cors = require("cors");
-const mongoose = require("mongoose");
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+const db = mongoose.connection;
 
 const app = express();
 app.use(cors());
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('Welcome to our Restaurant.');
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', (_) => {
+  console.log('Connected to DB');
 });
 
-const PORT = process.env.PORT || 8000;
+app.get('/', (req, res) => {
+  res.send('Hello to Restaurant.');
+});
 
-mongoose
-  .connect(process.env.DB_URL, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  })
-  .then(() => app.listen(PORT, () => console.log(`Listening on Port ${PORT}`)))
-  .catch((error) => console.error(error.message));
+module.exports = app;
