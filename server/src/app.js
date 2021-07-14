eb170364a8cf6d836d2b2f55a376505c970ffa43
Faisal Jamil <faisaljamilcs@gmail.com>
2021-07-15 01:57:15 +0530

    backend setup

---
+++ server/src/app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(cors());
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to our Restaurant.');
+});
+
+const PORT = process.env.PORT || 8000;
+
+mongoose
+  .connect(process.env.DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => app.listen(PORT, () => console.log(`Listening on Port ${PORT}`)))
+  .catch((error) => console.error(error.message));
